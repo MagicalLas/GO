bf38b2e8501ae8984fe8cccbc46e8351a670833a
La <35015234+Las-Wonho@users.noreply.github.com>
2018-04-23 20:49:17 +0900

    포인터 파일 추가. int는 0으로 초기화 됨.

---
+++ src/포인터.go
@@ -0,0 +1,16 @@
+package main
+
+import(
+	"fmt"
+)
+type two struct{
+	X int
+	Y int
+}
+func main(){
+	pointer := new(two)
+	fmt.Println(pointer)
+	pointer.X=5
+	pointer.Y=15
+	fmt.Println(pointer)
+}
