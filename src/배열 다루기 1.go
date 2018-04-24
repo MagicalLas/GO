b021239f4769d63481744bbe89e4e2fb2141de09
La <35015234+Las-Wonho@users.noreply.github.com>
2018-04-24 11:35:56 +0900

    슬라이싱 해보기

---
+++ src/배열 다루기 1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	arr := []int{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}
+	fmt.Println(arr)
+	fmt.Println("[n]",arr[1])
+	fmt.Println("[:n]",arr[:5])
+	fmt.Println("[n:]",arr[5:])
+	fmt.Println("[n:m]",arr[5:8])
+}
