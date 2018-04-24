d3227e9e303c9b900b42a984e010cd0acb8e67be
La <35015234+Las-Wonho@users.noreply.github.com>
2018-04-24 11:30:04 +0900

    구조체 생성하는 법

---
+++ src/구오체 접근.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+type trfle struct{
+	X int
+	Y string
+	Z float32
+}
+
+func main(){
+	str := trfle{Y:"las is good", X : 45, Z : 56.2}
+	fmt.Println(str)
+	fmt.Printf("%v",str)
+}
