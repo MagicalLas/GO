e14997e183456fa8d7aebedf35c3f61a70290122
La <35015234+Las-Wonho@users.noreply.github.com>
2018-04-25 08:33:44 +0900

    map make

---
+++ src/맵.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	m := make(map[string]int)
+	m["las"] = 5
+	m["la"] = 2
+
+	fmt.Println(m)
+
+	var mm = map[string]int{
+		"a":5,
+		"B":8,
+	}
+	fmt.Println(mm)
+}
