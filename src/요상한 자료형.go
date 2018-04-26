2db11e83951a7ee9985f82ca09f4f02fcc246c11
La <35015234+Las-Wonho@users.noreply.github.com>
2018-04-26 16:26:36 +0900

    complex 자료형 완벽 이해!

--- src/요상한 자료형.go
+++ src/요상한 자료형.go
@@ -5,6 +5,8 @@ func main(){
 		//허수???????? 그리고 이게 가능한건가... 대단해..
 		ex complex128 = 300000000000000<<400
 		a uintptr = 300000000000000<<15
+		exq complex128 = 44+454i
 	)
-	println(ex, a)
+	println(a ,ex)
+	println(exq)
 }
